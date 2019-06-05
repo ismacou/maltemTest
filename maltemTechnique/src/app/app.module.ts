8c52fd4c4e7c5bf78466c84c6af79e3cb768626a
Ismael Couvreur <ismael.couvreur@taleo-consulting.com>
2019-06-05 21:23:02 +0200

    add components

--- maltemTechnique/src/app/app.module.ts
+++ maltemTechnique/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movies/movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MoviesComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
